793a41f7413fecd0a40af9d1d6631be38b4bda88
narojay <hj200812@126.com>
2019-08-19 17:22:20 +0800

    Add project files.

---
+++ NarojayBlog.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NarojayBlog.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ NarojayBlog.Webapi/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NarojayBlog.Webapi.Controllers
+{
+
+    public class ValuesController : BaseController
+    {
+        // GET api/values
+        [HttpGet("values")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] {"value1", "value2"};
+        }
+    }
+}
